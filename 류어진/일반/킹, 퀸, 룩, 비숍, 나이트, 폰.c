207408cf300fbcec4dae358f21df9aad9e65f6ba
Ryueojin <dbdjwls3137@naver.com>
2022-08-16 14:22:19 +0900

    Update 킹, 퀸, 룩, 비숍, 나이트, 폰.c

--- 류어진/일반/킹, 퀸, 룩, 비숍, 나이트, 폰.c
+++ 류어진/일반/킹, 퀸, 룩, 비숍, 나이트, 폰.c
@@ -1,6 +1,44 @@
+//0입력후 if문을 받아들이지 못함
+
 #include<stdio.h>
 
 int main()
 {
-    printf("hello world");
+	int king, queen, l, b, n, p;
+	scanf_s("%d %d %d %d %d %d", &king, &queen, &l, &b, &n, &p);
+
+	king <= 10 && king >= 0;
+	queen <= 10 && queen >= 0;
+	l <= 10 && l >= 0;
+	b <= 10 && b >= 0;
+	n <= 10 && n >= 0;
+	p <= 10 && p >= 0;
+
+	if (king == 0) king = 1;
+	if (king == 1) king = 0;
+	if (king > 1) king = -(king - 1);
+	
+	if (queen == 0) queen = 1;
+	if (queen == 1) queen = 0;
+	if (queen > 1) queen = -(queen - 1);
+	
+	if (l < 2) l = 2 - l;
+	if (l == 2) l = 0;
+	if (l > 2) l = -(l - 2);
+
+	if (b < 2) b = 2 - b;
+	if (b == 2) b = 0;
+	if (b > 2) b = -(b - 2);
+
+	if (n < 2) n = 2 - n;
+	if (n == 2) n = 0;
+	if (n > 2) n = -(n - 2);
+
+	if (p < 8) p = 8 - p;
+	if (p == 8) p = 0;
+	if (p > 8) p = -(p - 8);
+
+	printf("%d %d %d %d %d %d", king, queen, l, b, n, p);
+
+	return 0;
 }
